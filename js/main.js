1ba91464ebed7ee411f748f8ab7b80185570fbb5
masaqqa <ma.saqqa@gmail.com>
2024-10-08 10:16:17 +0300

    add preloader to the template and do some optmizations

--- js/main.js
+++ js/main.js
@@ -1,24 +1,132 @@
 // Initialize Preloader
-$(window).on("load", function () {
-  $("#preloader").fadeOut("slow", function () {
-    $(this).remove();
-  });
-});
+(function ($) {
+  "use strict";
+  $(window).on("load", function () {
+    $("#preloader").fadeOut("slow", function () {
+      $(this).remove();
+    });
+  }, calcvulateNavbarTop());
+
+})(jQuery);
 
 // Change Navbar Top
-$(window).addEventListener("scroll", function () {
+window.addEventListener("scroll", calcvulateNavbarTop);
+
+function calcvulateNavbarTop () {
   const navbar = document.getElementById("navbar");
   const alert = document.getElementById("alert");
   const alertHeight = alert.clientHeight;
   const closeBtn = document.getElementById("closeAlert");
-  if (window.scrollY > alertHeight) {
-    navbar.style.top = 0;
+  if (alertHeight - window.scrollY >= 0) {
+    navbar.style.top =  `${alertHeight - window.scrollY}px`;
   } else {
-    navbar.style.top = `${alertHeight}px`;
+    navbar.style.top = 0;
   }
   closeBtn.addEventListener("click", () => {
     navbar.style.top = 0;
   });
+}
+
+// Initialize the Features Swiper
+const swiperFeatures = new Swiper(".swiper-features", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3000,  // Time between slides (in milliseconds)
+    disableOnInteraction: false,  // Keeps autoplay running after user interaction
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
-// Initialize the first Swiper
+// Initialize the Reviews Swiper
+const swiperReviews = new Swiper(".swiper-reviews", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3000,  // Time between slides (in milliseconds)
+    disableOnInteraction: false,  // Keeps autoplay running after user interaction
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// Initialize the Best Fares Swiper
+const swiperBestFares = new Swiper(".swiper-best-fares", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3000,  // Time between slides (in milliseconds)
+    disableOnInteraction: false,  // Keeps autoplay running after user interaction
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Initialize WOW Animated
+const WOW = new WOW().init();
