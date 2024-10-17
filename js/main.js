250855444ae7ca78c2605a520cef771518432879
masaqqa <ma.saqqa@gmail.com>
2024-10-17 16:24:34 +0300

    add new contact-us page and new mn-nhn page

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 // Initialize Preloader
 (function ($) {
   "use strict";
-  $(window).on("load", function () {
-    $("#preloader").fadeOut("slow", function () {
-      $(this).remove();
-    });
-  }, calcvulateNavbarTop());
-
+  $(window).on(
+    "load",
+    function () {
+      $("#preloader").fadeOut("slow", function () {
+        $(this).remove();
+      });
+    },
+    calcvulateNavbarTop()
+  );
 })(jQuery);
 
 // Change Navbar Top
 window.addEventListener("scroll", calcvulateNavbarTop);
 
-function calcvulateNavbarTop () {
+function calcvulateNavbarTop() {
   const navbar = document.getElementById("navbar");
   const alert = document.getElementById("alert");
   const alertHeight = alert.clientHeight;
   const closeBtn = document.getElementById("closeAlert");
   if (alertHeight - window.scrollY >= 0) {
-    navbar.style.top =  `${alertHeight - window.scrollY}px`;
+    navbar.style.top = `${alertHeight - window.scrollY}px`;
   } else {
     navbar.style.top = 0;
   }
@@ -27,14 +30,48 @@ function calcvulateNavbarTop () {
   });
 }
 
+// Toggle Classes for Collapse in the Search Page
+document.addEventListener("DOMContentLoaded", function () {
+  const collapseEditSearch = document.getElementById("collapseEditSearch");
+  const collapseFilter = document.getElementById("collapseFilter");
+  // Apply fade class only on mobile (smaller than 768px)
+  function applyFadeOnMobile() {
+    if (collapseEditSearch && collapseFilter) {
+      if (window.innerWidth < 768) {
+        collapseEditSearch.classList.remove("show");
+        collapseEditSearch.classList.add("fade");
+
+        collapseFilter.classList.remove("show");
+        collapseFilter.classList.add("fade");
+      } else {
+        collapseEditSearch.classList.remove("fade");
+        collapseEditSearch.classList.add("show");
+
+        collapseFilter.classList.remove("fade");
+        collapseFilter.classList.add("show");
+      }
+    }
+    return;
+  }
+
+  // Call the function when the page loads
+  applyFadeOnMobile();
+
+  // Handle window resize
+  window.addEventListener("resize", applyFadeOnMobile);
+});
+
+// Initialize WOW Animated
+new WOW().init();
+
 // Initialize the Features Swiper
 const swiperFeatures = new Swiper(".swiper-features", {
   slidesPerView: 3,
   spaceBetween: 30,
   loop: true,
   autoplay: {
-    delay: 3000,  // Time between slides (in milliseconds)
-    disableOnInteraction: false,  // Keeps autoplay running after user interaction
+    delay: 3000, // Time between slides (in milliseconds)
+    disableOnInteraction: false, // Keeps autoplay running after user interaction
   },
   breakpoints: {
     // when window width is >= 320px
@@ -68,8 +105,8 @@ const swiperReviews = new Swiper(".swiper-reviews", {
   spaceBetween: 30,
   loop: true,
   autoplay: {
-    delay: 3000,  // Time between slides (in milliseconds)
-    disableOnInteraction: false,  // Keeps autoplay running after user interaction
+    delay: 3000, // Time between slides (in milliseconds)
+    disableOnInteraction: false, // Keeps autoplay running after user interaction
   },
   breakpoints: {
     // when window width is >= 320px
@@ -99,8 +136,8 @@ const swiperBestFares = new Swiper(".swiper-best-fares", {
   spaceBetween: 30,
   loop: true,
   autoplay: {
-    delay: 3000,  // Time between slides (in milliseconds)
-    disableOnInteraction: false,  // Keeps autoplay running after user interaction
+    delay: 3000, // Time between slides (in milliseconds)
+    disableOnInteraction: false, // Keeps autoplay running after user interaction
   },
   breakpoints: {
     // when window width is >= 320px
@@ -127,6 +164,3 @@ const swiperBestFares = new Swiper(".swiper-best-fares", {
     prevEl: ".swiper-button-prev",
   },
 });
-
-// Initialize WOW Animated
-new WOW().init();
